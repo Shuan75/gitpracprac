6951671f893bf6909ab9c5c85cbc5f5d7a1dc8db
kmg0485 <kmg0485@naver.com>
2023-05-05 22:59:35 +0900

    Feat: init

---
+++ src/main/java/com/example/gitpracprac/GitpracpracApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitpracprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitpracpracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitpracpracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitpracprac/GitpracpracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitpracprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitpracpracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
